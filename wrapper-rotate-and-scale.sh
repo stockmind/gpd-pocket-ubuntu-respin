14f898772c72ac931bcef917503a30afd262299e
stockmind <slysimo@gmail.com>
2017-07-01 10:14:31 +0200

    Handle different display manager

--- wrapper-rotate-and-scale.sh
+++ wrapper-rotate-and-scale.sh
@@ -8,19 +8,31 @@ cp monitors.xml /usr/share/monitors.xml
 cp adduser.local /usr/local/sbin/adduser.local
 chmod +x /usr/local/sbin/adduser.local
 
-mkdir -p /var/lib/lightdm/.config/
-cp monitors.xml /var/lib/lightdm/.config/
-chown lightdm:lightdm /var/lib/lightdm/.config/monitors.xml
-
 mkdir -p /etc/X11/xorg.conf.d/
 cp 20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
 cp 90-monitor.conf /etc/X11/xorg.conf.d/90-monitor.conf
+
+# patch lightdm monitors config if folder exists
+if [ -d /var/lib/lightdm ]; then
+  mkdir -p /var/lib/lightdm/.config/
+  cp monitors.xml /var/lib/lightdm/.config/
+  chown lightdm:lightdm /var/lib/lightdm/.config/monitors.xml
+  chown lightdm:lightdm /var/lib/lightdm/.config
+fi
 
 # patch SDDM / KDE config if exist
 if [ -f /usr/share/sddm/scripts/Xsetup ]; then
   echo "xrandr --output DSI-1 --rotate right" >> /usr/share/sddm/scripts/Xsetup # Rotate Monitor0
   echo "xrandr --output DSI1 --rotate right" >> /usr/share/sddm/scripts/Xsetup # Rotate Monitor0
   echo "xrandr --dpi 168" >> /usr/share/sddm/scripts/Xsetup # Scaling 175%
+fi
+
+# patch GNOME 3 monitors config if folder exists
+if [ -d /var/lib/gdm3 ]; then
+  mkdir -p /var/lib/gdm3/.config/
+  cp monitors.xml /var/lib/gdm3/.config/
+  chown gdm:gdm /var/lib/gdm3/.config/monitors.xml
+  chown gdm:gdm /var/lib/gdm3/.config
 fi
 
 rm -f monitors.xml adduser.local 90x11-rotate_and_scale wrapper-rotate-and-scale.sh
