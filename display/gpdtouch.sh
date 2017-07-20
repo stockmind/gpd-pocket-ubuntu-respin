78c438b6325714c0ee2829d5a4423b4e1807715c
Simone <slysimo@gmail.com>
2017-07-20 02:55:10 +0200

    try three times

--- display/gpdtouch.sh
+++ display/gpdtouch.sh
@@ -5,19 +5,26 @@ DISPLAY=:0
 XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
 export DISPLAY XAUTHORITY
 
-SEARCH="Goodix Capacitive TouchScreen"
+# try at least 3 times to set correct transformation matrix
+for k in `seq 1 3`;
+do 
+	echo "trying $k..."
+	SEARCH="Goodix Capacitive TouchScreen"
 
-ids=$(xinput --list | awk -v search="$SEARCH" \
-    '$0 ~ search {match($0, /id=[0-9]+/);\
-                  if (RSTART) \
-                    print substr($0, RSTART+3, RLENGTH-3)\
-                 }'\
-     )
+	ids=$(xinput --list | awk -v search="$SEARCH" \
+	    '$0 ~ search {match($0, /id=[0-9]+/);\
+		          if (RSTART) \
+		            print substr($0, RSTART+3, RLENGTH-3)\
+		         }'\
+	     )
 
-# Rotate every instance of Touchscreen, this will workaround the ambiguity warning problem
-# that arise when calling xinput on "Goodix Capacitive Touchscreen"
-for i in $ids
-do
-    xinput set-prop $i "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
-done
+	# Rotate every instance of Touchscreen, this will workaround the ambiguity warning problem
+	# that arise when calling xinput on "Goodix Capacitive Touchscreen"
+	for i in $ids
+	do
+	    xinput set-prop $i "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
+	done
 
+	sleep 5
+	# wait for X loading on every try
+done   
