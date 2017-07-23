a7aa0decf9cac78ecb56dd2ea2315f550817a0be
stockmind <slysimo@gmail.com>
2017-07-23 19:31:43 +0200

    first tries with kali katoolin script

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 ISOFILE=$1
-WAYLAND=$2
+KALI=false
+KALICOMMANDS=""
+
+# Check arguments
+for i in "$@" ; do
+    if [[ $i == "wayland" ]] ; then
+        echo "Setting wayland monitors..."
+        WAYLAND=$i
+        break
+    fi
+    if [[ $i == "kali" ]] ; then
+    	echo "Setting kali environment..."	
+    	KALI=true
+    	break
+    fi	
+done
 
 if [ -n "$WAYLAND" ]; then
 	echo "Display setting: Wayland"
@@ -9,6 +24,21 @@ if [ -n "$WAYLAND" ]; then
 else
 	echo "Display setting: Xorg"
 	cp display/monitors_xorg.xml display/monitors.xml
+fi
+
+if [ "$KALI" = true ]; then
+	if [ ! -d katoolin ]; then
+		echo "Downloading katoolin repo..."
+		git clone https://github.com/LionSec/katoolin.git
+	fi
+
+	cd katoolin
+	echo "Update katoolin repo..."
+	git pull
+
+	$KALICOMMANDS=' -f distro/katoolin.sh -c katoolin.sh '
+
+	cd ..
 fi
 
 ./clean.sh
@@ -38,7 +68,7 @@ fi
 
 chmod +x isorespin.sh
 
-./isorespin.sh -i $ISOFILE -l "*.deb" -p "thermald tlp va-driver-all vainfo libva1 i965-va-driver gstreamer1.0-libav gstreamer1.0-vaapi vlc" -f display/20-intel.conf -f display/30-monitor.conf -f display/monitors.xml -f display/adduser.local -f display/90-scale -f display/90-touch -f display/gpdtouch.sh -f display/gpdtouch.service -f display/wrapper-display.sh -f audio/chtrt5645.conf -f audio/HiFi.conf -f audio/wrapper-audio.sh -f fan/gpdfand -f fan/gpdfand.conf -f fan/gpdfand.py -f fan/gpdfand.service -f fan/wrapper-fan.sh -f network/99-local-bluetooth.rules -f network/brcmfmac4356-pcie.txt -f network/wrapper-network.sh -f power/wrapper-power.sh -c wrapper-audio.sh -c wrapper-display.sh -c wrapper-fan.sh -c wrapper-network.sh -c wrapper-power.sh -g "" -g "i915.fastboot=1 i915.semaphores=1"
+./isorespin.sh -i $ISOFILE -l "*.deb" -p "thermald tlp va-driver-all vainfo libva1 i965-va-driver gstreamer1.0-libav gstreamer1.0-vaapi vlc" -f display/20-intel.conf -f display/30-monitor.conf -f display/monitors.xml -f display/adduser.local -f display/90-scale -f display/90-touch -f display/gpdtouch.sh -f display/gpdtouch.service -f display/wrapper-display.sh -f audio/chtrt5645.conf -f audio/HiFi.conf -f audio/wrapper-audio.sh -f fan/gpdfand -f fan/gpdfand.conf -f fan/gpdfand.py -f fan/gpdfand.service -f fan/wrapper-fan.sh -f network/99-local-bluetooth.rules -f network/brcmfmac4356-pcie.txt -f network/wrapper-network.sh -f power/wrapper-power.sh -c wrapper-audio.sh -c wrapper-display.sh -c wrapper-fan.sh -c wrapper-network.sh -c wrapper-power.sh $KALICOMMANDS -g "" -g "i915.fastboot=1 i915.semaphores=1"
 
 
 
