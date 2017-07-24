c3beb5d994fe8d650d8ebe2836374c85a448fe11
stockmind <slysimo@gmail.com>
2017-07-24 18:18:01 +0200

    Update gpdtouch.sh

--- display/gpdtouch.sh
+++ display/gpdtouch.sh
@@ -11,20 +11,11 @@ do
 	echo "trying $k..."
 	SEARCH="Goodix Capacitive TouchScreen"
 
-	ids=$(xinput --list | awk -v search="$SEARCH" \
-	    '$0 ~ search {match($0, /id=[0-9]+/);\
-		          if (RSTART) \
-		            print substr($0, RSTART+3, RLENGTH-3)\
-		         }'\
-	     )
+	id=$(xinput list --id-only pointer:"Goodix Capacitive TouchScreen")
 
 	# Rotate every instance of Touchscreen, this will workaround the ambiguity warning problem
 	# that arise when calling xinput on "Goodix Capacitive Touchscreen"
-	for i in $ids
-	do
-	    xinput set-prop $i "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
-	    break # Take only first match
-	done
+	xinput set-prop $id "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
 	
 	# try also to rotate display if monitors file gets ignored
 	xrandr --output DSI1 --rotate right
