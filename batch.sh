f0dcf549f38139454d0f3c8a481e28d90f6993c2
simonerobertonunzi <slysimo@gmail.com>
2017-08-30 03:20:01 +0200

    first attempt for batch generation script

---
+++ batch.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Generate iso in batch from folder
+
+mkdir origin
+mkdir destination
+
+# Remove *.iso from found files
+shopt -s nullglob
+for FILE in origin/*.iso; do
+
+	NOW=$(date +"%Y%m%d")
+
+	# Remove path from file
+	FILECLEAN="${FILE##*/}"
+	# Find extension
+	FILEXT="${FILECLEAN##*.}"
+	# Get file name without extension, look until last dot
+	FILENAME="${FILECLEAN%.*}"
+	echo $FILENAME
+	# No spaces
+	FILENAME="${FILENAME// /-}"
+	echo $FILENAME
+	# Only one dash
+	FILENAME="$(echo $FILENAME | tr -s '-')"
+	# Sanitize name
+	FILENAME="${FILENAME//[^[:alnum:]_-]/}"
+
+	if [[ $string == *"gnome"* ]]; then
+		echo "Gnome ISO"
+		./build.sh "$FILE" gnome
+	else
+		echo "Standard ISO"	
+		./build.sh "$FILE" 
+	fi
+
+	mv "linuxium-$FILECLEAN" "destination/gpdpocket-$NOW-$FILECLEAN"
+done
