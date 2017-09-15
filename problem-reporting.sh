9d5edd6869cc8ac7aa43c37f357d6fa51efc07ab
simone <slysimo@gmail.com>
2017-09-16 01:35:05 +0200

    updated error reporting script

--- problem-reporting.sh
+++ problem-reporting.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
 
 # GPD Pocket system environment check
 # Author: Simone Roberto Nunzi aka stockmind
@@ -8,6 +13,6 @@ echo "Kernel: $(uname -r)"
 echo "Desktop Environment: $XDG_CURRENT_DESKTOP"
 echo "Display: $DISPLAY"
 echo "Monitor: $(xrandr | grep DSI1)"
-echo "Scripts: $(ls /usr/local/sbin/)"
-echo "EFI: $(dmesg | grep \"EFI v\")"
-echo "EFI Release date: $(dmidecode | grep \"Release\")"
+echo "Scripts: " $(ls /usr/local/sbin/ | tr "\n" " ")
+echo "EFI: " $(dmesg | grep "EFI v")
+echo "EFI " $(dmidecode | grep "Release")
