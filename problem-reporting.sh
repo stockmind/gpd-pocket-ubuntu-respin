7b22aa8a5d8de59d4e7c0eb324956f70c78725af
stockmind <slysimo@gmail.com>
2017-09-16 02:44:09 +0200

    Add some markup

--- problem-reporting.sh
+++ problem-reporting.sh
@@ -9,10 +9,10 @@ fi
 # Author: Simone Roberto Nunzi aka stockmind
 # Problem reporting script
 
-echo "Kernel: $(uname -r)"
-echo "Desktop Environment: $XDG_CURRENT_DESKTOP"
-echo "Display: $DISPLAY"
-echo "Monitor: $(xrandr | grep DSI1)"
-echo "Scripts: " $(ls /usr/local/sbin/ | tr "\n" " ")
-echo "EFI: " $(dmesg | grep "EFI v")
-echo "EFI " $(dmidecode | grep "Release")
+echo "**Kernel:** $(uname -r)"
+echo "**Desktop Environment:** $XDG_CURRENT_DESKTOP"
+echo "**Display:** $DISPLAY"
+echo "**Monitor:** $(xrandr | grep DSI1)"
+echo "**Scripts:** " $(ls /usr/local/sbin/ | tr "\n" " ")
+echo "**EFI:** " $(dmesg | grep "EFI v")
+echo "**EFI** " $(dmidecode | grep "Release")
