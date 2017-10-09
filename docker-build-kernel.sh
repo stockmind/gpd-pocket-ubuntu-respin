06af667edb30dcc75d40511c5b57b859f9bbbbd4
stockmind <slysimo@gmail.com>
2017-10-09 12:56:08 +0200

    Update docker-build-kernel.sh

--- docker-build-kernel.sh
+++ docker-build-kernel.sh
@@ -8,7 +8,18 @@ OUTPUTDIR="$SCRIPTPATH""/destination"
 echo "Input dir: $INPUTDIR"
 echo "Output dir: $OUTPUTDIR"
 
+if [[ $(docker inspect --type=image stockmind/gpd-pocket-ubuntu-respin) == 0 ]]; then
+	echo "Found Docker Hub image!"
+	IMAGENAME="stockmind/gpd-pocket-ubuntu-respin"
+elif [[ $(docker inspect --type=image gpd-pocket-ubuntu-respin) == 0 ]]; then
+	echo "Found local image!"
+	IMAGENAME="gpd-pocket-ubuntu-respin"
+else
+	echo "Docker image not found! Build docker image or download it from Docker Hub!"
+	exit 1
+fi
+
 # Refresh container
 docker rm $(docker ps -aq --filter name=gpd-pocket-kernel-container)
 # Run command
-docker run -t -v "$INPUTDIR":/docker-input -v "$OUTPUTDIR":/docker-output --name gpd-pocket-kernel-container gpd-pocket-ubuntu-respin kernel
+docker run -t -v "$INPUTDIR":/docker-input -v "$OUTPUTDIR":/docker-output --name gpd-pocket-kernel-container "$IMAGENAME" kernel
