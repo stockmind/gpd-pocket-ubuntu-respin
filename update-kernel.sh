3e12d30afd71e7a4d4cdbba4aec54cdd2ffbe6a9
stockmind <slysimo@gmail.com>
2017-10-10 20:46:57 +0200

    Kernel with fixed audio crackling

--- update-kernel.sh
+++ update-kernel.sh
@@ -3,13 +3,13 @@
 mkdir -p update-kernel
 cd update-kernel
 
-if [ ! -f gpdpocket-20171007-kernel-files.zip ]; then
+if [ ! -f gpdpocket-20171010-kernel-files-audio-fix.zip ]; then
 	 echo "Downloading kernel files...."
-	 wget https://bitbucket.org/simone_nunzi/gpdpocket-kernel/downloads/gpdpocket-20171007-kernel-files.zip
+	 wget https://bitbucket.org/simone_nunzi/gpdpocket-kernel/downloads/gpdpocket-20171010-kernel-files-audio-fix.zip
 fi
 
 echo "Extracting kernel files..."
-unzip -o gpdpocket-20171007-kernel-files.zip
+unzip -o gpdpocket-20171010-kernel-files-audio-fix.zip
 
 echo "Installing kernel..."
 sudo dpkg -i *.deb
