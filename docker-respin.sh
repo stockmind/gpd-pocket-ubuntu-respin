73efab11f483ac2ed09e3cabeac205b4011a5bbd
stockmind <slysimo@gmail.com>
2018-01-03 13:13:55 +0100

    improved code

--- docker-respin.sh
+++ docker-respin.sh
@@ -4,6 +4,9 @@
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 INPUTDIR="$SCRIPTPATH""/origin"
 OUTPUTDIR="$SCRIPTPATH""/destination"
+ORIGINDIR="origin/"
+ISO=${1#$ORIGINDIR} # Remove 'origin/' prefix path if found
+PAR1=$2
 
 if $(docker image inspect stockmind/gpd-pocket-ubuntu-respin:latest >/dev/null 2>&1); then
 	echo "Found Docker Hub image!"
@@ -16,10 +19,11 @@ else
 	exit 1
 fi
 
+echo "Iso: $ISO"
 echo "Input dir: $INPUTDIR"
 echo "Output dir: $OUTPUTDIR"
 
 # Refresh container
 docker rm $(docker ps -aq --filter name=gpd-pocket-respin-container)
 # Run command
-docker run -t --cap-add MKNOD -v "$INPUTDIR":/docker-input -v "$OUTPUTDIR":/docker-output --privileged --name gpd-pocket-respin-container "$IMAGENAME" respin $1 $2
+docker run -t --cap-add MKNOD -v "$INPUTDIR":/docker-input -v "$OUTPUTDIR":/docker-output --privileged --name gpd-pocket-respin-container "$IMAGENAME" respin $ISO $PAR1
