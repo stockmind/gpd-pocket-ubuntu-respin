f21e42b6623b7591d48cd7b58edae4002e0613f7
stockmind <slysimo@gmail.com>
2018-02-07 04:19:47 +0100

    Curl is better!

--- kernel/compareconfig.sh
+++ kernel/compareconfig.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/jwrdegoede/linux-sunxi/master/.config -O jwrdegoede-config
-wget https://raw.githubusercontent.com/stockmind/gpd-pocket-ubuntu-respin/master/kernel/.config -O stockmind-config
-wget https://raw.githubusercontent.com/petrmatula190/gpd-pocket-kernel/master/.config -O petrmatula190-config
+curl https://raw.githubusercontent.com/jwrdegoede/linux-sunxi/master/.config -o jwrdegoede-config
+curl https://raw.githubusercontent.com/stockmind/gpd-pocket-ubuntu-respin/master/kernel/.config -o stockmind-config
+curl https://raw.githubusercontent.com/petrmatula190/gpd-pocket-kernel/master/.config -o petrmatula190-config
 
 # Compare config to check new flags that may be missing 
 diff -u stockmind-config jwrdegoede-config > config-stockmind.patch
