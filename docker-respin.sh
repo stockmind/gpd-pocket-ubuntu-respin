062b51502940434d59915d39f96f79a883649bdb
stockmind <slysimo@gmail.com>
2018-04-20 19:50:14 +0200

    Fix multiple arguments

--- docker-respin.sh
+++ docker-respin.sh
@@ -6,9 +6,6 @@ INPUTDIR="$SCRIPTPATH""/origin"
 OUTPUTDIR="$SCRIPTPATH""/destination"
 ORIGINDIR="origin/"
 ISO=${1#$ORIGINDIR} # Remove 'origin/' prefix path if found
-PAR1=$2
-PAR2=$3
-PAR3=$4
 
 if $(docker image inspect stockmind/gpd-pocket-ubuntu-respin:latest >/dev/null 2>&1); then
 	echo "Found Docker Hub image!"
@@ -28,4 +25,4 @@ echo "Output dir: $OUTPUTDIR"
 # Refresh container
 docker rm $(docker ps -aq --filter name=gpd-pocket-respin-container)
 # Run command
-docker run -t --cap-add MKNOD -v "$INPUTDIR":/docker-input -v "$OUTPUTDIR":/docker-output --privileged --name gpd-pocket-respin-container "$IMAGENAME" respin $ISO $PAR1 $PAR2 $PAR3
+docker run -t --cap-add MKNOD -v "$INPUTDIR":/docker-input -v "$OUTPUTDIR":/docker-output --privileged --name gpd-pocket-respin-container "$IMAGENAME" respin $ISO "${@:2}"
