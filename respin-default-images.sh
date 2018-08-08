49580ce599694d5155d70f965c020c3f15b8bda4
stockmind <slysimo@gmail.com>
2018-08-08 13:28:15 +0200

    updated recipe

--- respin-default-images.sh
+++ respin-default-images.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-./docker-respin.sh origin/ubuntu-18.04-beta2-desktop-amd64.iso gnome bionicbeaver
-./docker-respin.sh origin/ubuntu-17.10.1-desktop-amd64.iso gnome
-./docker-respin.sh origin/ubuntu-17.10.1-desktop-amd64.iso unity
-./docker-respin.sh origin/linuxmint-18.3-mate-64bit.iso
-./docker-respin.sh origin/linuxmint-18.3-cinnamon-64bit.iso
+./docker-respin.sh origin/ubuntu-18.04.1-desktop-amd64.iso gnome mainline upgrade bionicbeaver
+./docker-respin.sh origin/ubuntu-18.04.1-desktop-amd64.iso unity mainline upgrade bionicbeaver
